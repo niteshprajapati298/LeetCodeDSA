ce8946d7d299028436e4d794c5cc0a5f7bff2209
Nitesh Prajapati <160471977+niteshprajapati298@users.noreply.github.com>
2026-07-27 22:29:17 +0530

    Time: 0 ms (100%), Space: 20.8 MB (51.59%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,35 +1,88 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+
+        // extra space ;
+        //     int m = matrix.size();
+        //     int n = matrix[0].size();
+        //     vector<int> row(m, 0);
+        //     vector<int> col(n, 0);
+        //     for (int i = 0; i < m; i++) {
+        //         for (int j = 0; j < n; j++) {
+        //             if (matrix[i][j] == 0) {
+        //                 row[i] = 1;
+        //                 col[j] = 1;
+        //             }
+        //         }
+        //     }
+        //    for(int i = 0; i < row.size(); i++){
+        //       if(row[i]==1){
+        //        int j = 0;
+        //        while(j < n){
+        //          matrix[i][j] = 0;
+        //           j++;
+        //        }
+        //       }
+        //    }
+        //     for(int i = 0; i < col.size(); i++){
+        //       if(col[i]==1){
+        //        int j = 0;
+        //        while(j < m){
+        //          matrix[j][i] = 0;
+        //           j++;
+        //        }
+        //       }
+        //    }
+
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<int> row(m, 0);
-        vector<int> col(n, 0);
+        bool firstCol = false;
+
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+            if (matrix[i][0] == 0)
+                firstCol = true;
+        }
+        bool isFirstIndexAffected = false;
+        if(matrix[0][0] == 0) isFirstIndexAffected = true;
+ 
+        // vector<int> row(m, 0);
+        // vector<int> col(n, 0);
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    row[i] = 1;
-                    col[j] = 1;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-       for(int i = 0; i < row.size(); i++){
-          if(row[i]==1){
-           int j = 0;
-           while(j < n){
-             matrix[i][j] = 0;
-              j++;
-           }
-          }
-       }
-        for(int i = 0; i < col.size(); i++){
-          if(col[i]==1){
-           int j = 0;
-           while(j < m){
-             matrix[j][i] = 0;
-              j++;
-           }
-          }
-       }
+
+        for (int i = 1; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                int j = 1;
+                while (j < n) {
+                    matrix[i][j] = 0;
+                    j++;
+                }
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                int j = 1;
+                while (j < m) {
+                    matrix[j][i] = 0;
+                    j++;
+                }
+            }
+        }
+if (matrix[0][0] == 0) {
+    for (int j = 0; j < n; j++) {
+        matrix[0][j] = 0;
+    }
+}
+        if (firstCol) {
+    for (int i = 0; i < m; i++) {
+        matrix[i][0] = 0;
+    }
+}
     }
 };
