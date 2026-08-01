56124c319e216de4c0ee3216aeaf152ec7daacb1
Nitesh Prajapati <160471977+niteshprajapati298@users.noreply.github.com>
2026-08-01 21:57:29 +0530

    Time: 0 ms (100%), Space: 10.2 MB (45.39%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+
+    int solve(int i, int j, vector<int>& nums) {
+
+        if (i == j)
+            return nums[i];
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int takeLeft = nums[i] - solve(i + 1, j, nums);
+
+        int takeRight = nums[j] - solve(i, j - 1, nums);
+
+        return dp[i][j] = max(takeLeft, takeRight);
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+
+        int n = nums.size();
+
+        dp.assign(n, vector<int>(n, -1));
+
+        return solve(0, n - 1, nums) >= 0;
+    }
+};
