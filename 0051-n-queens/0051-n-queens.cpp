7c91ce89198f4f319276c07b1d9532a09c0e344a
Nitesh Prajapati <160471977+niteshprajapati298@users.noreply.github.com>
2026-08-18 08:46:46 +0530

    Time: 10 ms (12.98%), Space: 15.8 MB (8.31%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,70 +1,54 @@
 class Solution {
 public:
-    vector<vector<string>>ans;
+   bool isSafe(vector<string>& board, int row, int col, int n) {
 
-    bool isSafe(int row, int col, vector<string>& board, int n) {
-
-        // Check upper-left diagonal
-        int r = row, c = col;
-        while (r >= 0 && c >= 0) {
-            if (board[r][c] == 'Q')
-                return false;
-            r--;
-            c--;
-        }
-
-        // Check same column
-        r = row;
-        while (r >= 0) {
-            if (board[r][col] == 'Q')
-                return false;
-            r--;
-        }
-
-        // Check upper-right diagonal
-        r = row;
-        c = col;
-        while (r >= 0 && c < n) {
-            if (board[r][c] == 'Q')
-                return false;
-            r--;
-            c++;
-        }
-
-        return true;
+    // Check upper cells in same column
+    for (int i = row - 1; i >= 0; i--) {
+        if (board[i][col] == 'Q')
+            return false;
     }
 
-    void solve(int row, vector<string>& board, int n) {
+    // Check upper-right diagonal
+    for (int i = row - 1, j = col + 1;
+         i >= 0 && j < n;
+         i--, j++) {
 
-        // Base Case
-        if (row == n) {
+        if (board[i][j] == 'Q')
+            return false;
+    }
+
+    // Check upper-left diagonal
+    for (int i = row - 1, j = col - 1;
+         i >= 0 && j >= 0;
+         i--, j--) {
+
+        if (board[i][j] == 'Q')
+            return false;
+    }
+
+    return true;
+}
+    void solve(vector<string> board, vector<vector<string>>&ans, int row,
+               int n) {
+        if (row >= n) {
             ans.push_back(board);
             return;
         }
 
-        // Try every column in the current row
         for (int col = 0; col < n; col++) {
 
-            if (isSafe(row, col, board, n)) {
-
-                // Place Queen
+            // check is safe
+            if (isSafe(board, row, col, n)) {
                 board[row][col] = 'Q';
-
-                // Solve for next row
-                solve(row + 1, board, n);
-
-                // Backtrack
+                solve(board, ans, row + 1, n);
                 board[row][col] = '.';
             }
         }
     }
-
     vector<vector<string>> solveNQueens(int n) {
-
+        vector<vector<string>> ans;
         vector<string> board(n, string(n, '.'));
-
-        solve(0, board, n);
-
+        solve(board, ans, 0, n);
         return ans;
     }
 };
