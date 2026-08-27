1d9065e2e7fa1ce7495fb01002b999a5f1865ab5
Nitesh Prajapati <160471977+niteshprajapati298@users.noreply.github.com>
2026-08-27 23:10:56 +0530

    Time: 3 ms (68.13%), Space: 13.1 MB (82.73%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,37 +1,46 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        int count = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if(count < 0) return false;
+    // bool isValid(string s) {
+    //     int count = 0;
+    //     for (int i = 0; i < s.length(); i++) {
+    //         if (count < 0)
+    //             return false;
 
-            if (s[i] == '(') {
-                count++;
-            } else if (s[i] == ')') {
-                count--;
-            }
-        }
-        return count == 0 ? true : false;
-    }
-    void solve(int &n, string& s, vector<string>& ans) {
+    //         if (s[i] == '(') {
+    //             count++;
+    //         } else if (s[i] == ')') {
+    //             count--;
+    //         }
+    //     }
+    //     return count == 0 ? true : false;
+    // }
+    void solve(int& n, string& s, vector<string>& ans, int &close, int& open) {
         if (s.length() == n * 2) {
-            if (isValid(s)) {
                 ans.push_back(s);
                 return;
-            }
-            else return;
         }
-        s.push_back('(');
-        solve(n,s, ans);
-        s.pop_back();
-        s.push_back(')');
-        solve(n,s , ans);
-        s.pop_back();
+
+        if (open < n) {
+            s.push_back('(');
+            open = open + 1;
+            solve(n, s, ans, close, open);
+            open = open - 1;
+            s.pop_back();
+        }
+
+        if (close < open) {
+            s.push_back(')');
+            close = close + 1;
+            solve(n, s, ans, close, open);
+            close = close-1;
+            s.pop_back();
+        }
     }
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
         string s = "";
-        solve(n, s, ans);
+        int close = 0 , open = 0;
+        solve(n, s, ans , close, open);
         return ans;
     }
 };
