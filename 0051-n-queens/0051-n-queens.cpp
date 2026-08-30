01c5807695c1f57e6967583e27330ae11992caa9
Nitesh Prajapati <160471977+niteshprajapati298@users.noreply.github.com>
2026-08-31 01:14:08 +0530

    Time: 319 ms (5.71%), Space: 10 MB (99.73%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,80 +1,71 @@
 class Solution {
 public:
-    // bool isSafe(vector<string>& board, int row, int col, int n) {
+    bool isValid(int row, int col, vector<string>& board) {
+        // check the cols && rows
+         int n = board[0].size();
+          // Same column
+    for (int i = row - 1; i >= 0; i--) {
+        if (board[i][col] == 'Q') {
+            return false;
+        }
+    }
+        int i = row - 1;
+        int j = col - 1;
 
-    //     // Check upper cells in same column
-    //     for (int i = row - 1; i >= 0; i--) {
-    //         if (board[i][col] == 'Q')
-    //             return false;
-    //     }
+        // left upper diagonal
+        while (i >= 0 && j >= 0) {
+            if (board[i][j] == 'Q') {
+                cout << "Returnni false b2 " << endl;
+                return false;
+            }
+            i--;
+            j--;
+        }
+        i = row - 1;
+        j = col + 1;
 
-    //     int i = row - 1;
-    //     int j = col + 1;
+        while (i >= 0 && j < n) {
+            if (board[i][j] == 'Q') {
+                cout << "Returnni false b3 " << endl;
 
-    //     while (i >= 0 && j < n) {
+                return false;
+            }
+            j++;
+            i--;
+        }
 
-    //         if (board[i][j] == 'Q')
-    //             return false;
+        // right upper diagonal
 
-    //         i--;
-    //         j++;
-    //     }
+        return true;
+    }
 
-    //      i = row - 1;
-    //      j = col - 1;
+    void solve(int& n, int row, vector<string>& board,
+               vector<vector<string>>& ans) {
 
-    //     while (i >= 0 && j >= 0) {
-
-    //         if (board[i][j] == 'Q')
-    //             return false;
-
-    //         i--;
-    //         j--;
-    //     }
-
-    //     return true;
-    // }
-    void solve(vector<string> board, vector<vector<string>>& ans, int row,
-               int n, unordered_set<int>& cols, unordered_set<int>& diag,
-               unordered_set<int>& antiDiag) {
         if (row >= n) {
             ans.push_back(board);
             return;
         }
-
         for (int col = 0; col < n; col++) {
-            int diagConstant = row + col;
-            int antiDiagConstant = row - col;
-            if (cols.find(col) != cols.end() ||
-                diag.find(diagConstant) != diag.end() ||
-                antiDiag.find(antiDiagConstant) != antiDiag.end()) {
-                continue;
-            }
-            cols.insert(col);
-            diag.insert(diagConstant);
-            antiDiag.insert(antiDiagConstant);
-            board[row][col] = 'Q';
-            solve(board, ans, row + 1, n, cols, diag, antiDiag);
-            cols.erase(col);
-            diag.erase(diagConstant);
-            antiDiag.erase(antiDiagConstant);
-            board[row][col] = '.';
+            bool canPlace = isValid(row, col, board);
+            cout << " canPlace " << canPlace << endl;
+            if (canPlace) {
+                // check valid hai ya nahi
+                board[row][col] = 'Q';
 
-            // // check is safe
-            // if (isSafe(board, row, col, n)) {
-            //     board[row][col] = 'Q';
-            //     solve(board, ans, row + 1, n);
-            //     board[row][col] = '.';
-            // }
+                // next ke liye check karo
+                solve(n, row+1, board, ans);
+
+                // revert back all the shells
+                board[row][col] = '.';
+            }
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
         vector<string> board(n, string(n, '.'));
-        unordered_set<int> cols;
-        unordered_set<int> diag;
-        unordered_set<int> antiDiag;
-        solve(board, ans, 0, n, cols, diag, antiDiag);
+        int row = 0;
+        solve(n, row, board, ans);
         return ans;
     }
 };
